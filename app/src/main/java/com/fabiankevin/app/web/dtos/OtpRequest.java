a6970e15d7d6e33a1d2e2a7b1524c44347bf75e4
kev <fbian.kevin@gmail.com>
2025-04-30 19:38:03 +0800

    Add @Schema annotation to `OtpRequest.userIdentifier` for description, examples, and max length

--- app/src/main/java/com/fabiankevin/app/web/dtos/OtpRequest.java
+++ app/src/main/java/com/fabiankevin/app/web/dtos/OtpRequest.java
@@ -4,6 +4,7 @@ import com.fabiankevin.app.models.enums.DeliveryMethod;
 import com.fabiankevin.app.models.enums.OtpPurpose;
 import com.fabiankevin.app.services.commands.GenerateOtpCommand;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -12,6 +13,12 @@ import lombok.Builder;
 public record OtpRequest(
         @NotBlank(message = "User identifier is required")
         @JsonProperty("user_identifier")
+        @Schema(description = "User identifier could be an email or phone number",
+                examples = {
+                        "test@example.com",
+                        "+639000000"
+                },
+                maxLength = 128)
         String userIdentifier,
         @NotNull(message = "OTP purpose must be specified")
         OtpPurpose purpose,
