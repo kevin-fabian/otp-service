8fb56542baa6c6ea62a73aa6cf5bc9a39c8dd89b
kev <fbian.kevin@gmail.com>
2025-07-19 13:35:48 +0800

    Add TOTP code verifier tests and improve validation
    - Implement unit tests for `DefaultTotpCodeVerifier`
    - Validate `secret` and `totpCode` in `verify` method to handle null or empty inputs

--- app/src/main/java/com/fabiankevin/app/services/DefaultTotpCodeVerifier.java
+++ app/src/main/java/com/fabiankevin/app/services/DefaultTotpCodeVerifier.java
@@ -4,6 +4,8 @@ import dev.samstevens.totp.code.CodeVerifier;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
 public class DefaultTotpCodeVerifier implements TotpCodeVerifier {
@@ -11,6 +13,15 @@ public class DefaultTotpCodeVerifier implements TotpCodeVerifier {
 
     @Override
     public boolean verify(String secret, String totpCode) {
+        boolean validSecret = Optional.ofNullable(secret)
+                .filter(secretParam -> !secretParam.isBlank()).isPresent();
+        boolean validTotpCode = Optional.ofNullable(totpCode)
+                .filter(code -> !code.isBlank()).isPresent();
+
+        if(!validSecret || !validTotpCode) {
+            return false;
+        }
+
         return codeVerifier.isValidCode(secret, totpCode);
     }
 }
